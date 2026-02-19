027581e89b049196f634bdeec9545e7e47815339
Svitlana <lanamartinenko180@gmail.com>
2026-02-19 14:36:25 +0200

    save

--- js/task-1.js
+++ js/task-1.js
@@ -3,7 +3,7 @@ function makeTransaction (quantity, pricePerDroid) {
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`
 }
 
-makeTransaction ()
+makeTransaction();
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
