0ac6ce3ffa74ff0ce39faae4ca3d8e31fdeb2ba1
Svitlana <lanamartinenko180@gmail.com>
2026-02-19 15:16:53 +0200

    save

--- js/task-3.js
+++ js/task-3.js
@@ -1,3 +1,13 @@
 function getElementWidth(content, padding, border) {
-    
+    const contentNum = Number.parseFloat(content);
+    const paddingNum = Number.parseFloat(padding);
+    const borderNum = Number.parseFloat(border);
+    const totalWidth = contentNum + 2 * paddingNum + 2 * borderNum;
+    return totalWidth;
 }
+
+getElementWidth();
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
